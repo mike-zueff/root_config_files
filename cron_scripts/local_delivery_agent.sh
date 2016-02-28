be0def0313be23b4f973cd3308e490c9c54d7fa8
Mike Zueff <mike.zueff@gmail.com>
2016-02-28 14:53:09 +0300

    Moved cron scripts and installer to /root.

---
+++ cron_scripts/local_delivery_agent.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+. /root/cron_scripts/private_rc_file.sh
+
+for FILE in `ls /var/nullmailer/queue`
+do
+  chmod 0777 /var/nullmailer/queue/${FILE}
+  chown ${_RC_ADDRESSEE}:${_RC_ADDRESSEE} /var/nullmailer/queue/${FILE}
+  mv --force /var/nullmailer/queue/${FILE} /home/${_RC_ADDRESSEE}/.maildir/cur
+done
---
+++ cron_scripts/scripts_installer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ln --force --symbolic /root/cron_scripts/local_delivery_agent.sh /etc/cron.hourly/local_delivery_agent
