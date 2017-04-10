a6e9b5b90b4878ba9a25b93139b3eff235f687eb
Mike Zueff <mike.zueff@gmail.com>
2017-04-10 16:13:15 +0300

    Updated local delivery agent.

--- .config/cron_scripts/local_delivery_agent.sh
+++ .config/cron_scripts/local_delivery_agent.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+NULLMAILER_QUEUE="/var/spool/nullmailer/queue"
+
 . /root/.config/cron_scripts/private_rc_file.sh
 
-for FILE in `ls /var/nullmailer/queue`
+for FILE in `ls "${NULLMAILER_QUEUE}"`
 do
-  sed --in-place "1,3d" /var/nullmailer/queue/${FILE}
-  chmod 0777 /var/nullmailer/queue/${FILE}
-  chown ${_RC_ADDRESSEE}:${_RC_ADDRESSEE} /var/nullmailer/queue/${FILE}
-  mv --force /var/nullmailer/queue/${FILE} /home/${_RC_ADDRESSEE}/.maildir/cur
+  chmod 0777 "${NULLMAILER_QUEUE}/${FILE}"
+  chown ${_RC_ADDRESSEE}:${_RC_ADDRESSEE} "${NULLMAILER_QUEUE}/${FILE}"
+  head --lines=1 "${NULLMAILER_QUEUE}/${FILE}" | grep --quiet '.@defaulthost\.defaultdomain$'
+  [[ ${?} -eq 0 ]] && sed --in-place '1,3d' "${NULLMAILER_QUEUE}/${FILE}"
+  mv --force "${NULLMAILER_QUEUE}/${FILE}" "/home/${_RC_ADDRESSEE}/.maildir/cur"
 done
