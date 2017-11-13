234032e11e1cc9f5e56e6fa7e0ce48243eca4063
Mike Zueff <mike.zueff@gmail.com>
2017-11-13 03:14:18 +0300

    Fixed a cron script.

--- .config/cron_scripts/local_delivery_agent.sh
+++ .config/cron_scripts/local_delivery_agent.sh
@@ -7,7 +7,7 @@ for FILE in `ls "${NULLMAILER_QUEUE}"`
 do
   chmod 0777 "${NULLMAILER_QUEUE}/${FILE}"
   chown ${_RC_PRINCIPAL_USER}:${_RC_PRINCIPAL_USER} "${NULLMAILER_QUEUE}/${FILE}"
-  head --lines=1 "${NULLMAILER_QUEUE}/${FILE}" | grep --quiet '.@defaulthost\.defaultdomain$'
+  head --lines=1 "${NULLMAILER_QUEUE}/${FILE}" | grep --quiet '\.defaultdomain$'
   [[ ${?} -eq 0 ]] && sed --in-place '1,3d' "${NULLMAILER_QUEUE}/${FILE}"
   mv --force "${NULLMAILER_QUEUE}/${FILE}" "/home/${_RC_PRINCIPAL_USER}/.maildir/cur"
 done
