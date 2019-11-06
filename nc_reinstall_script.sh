35af7b1c35485879dd757748b4ba03615078e0d7
Mike Zueff <mike.zueff@gmail.com>
2019-11-07 00:49:06 +0300

    Update NC reinstall script

--- nc_reinstall_script.sh
+++ nc_reinstall_script.sh
@@ -1,39 +1,4 @@
-#!/bin/bash
-ncpath='/var/www/localhost/htdocs/nextcloud'
-htuser='nginx'
-htgroup='nginx'
-rootuser='root'
-
-chown -R nginx. ${ncpath}
-
-#printf "Creating possible missing Directories\n"
-mkdir -p $ncpath/data
-mkdir -p $ncpath/assets
-mkdir -p $ncpath/updater
-
-#printf "chmod Files and Directories\n"
-find ${ncpath}/ -type f -print0 | xargs -0 chmod 0640
-find ${ncpath}/ -type d -print0 | xargs -0 chmod 0750
-
-#printf "chown Directories\n"
-chown -R ${rootuser}:${htgroup} ${ncpath}/
-chown -R ${htuser}:${htgroup} ${ncpath}/apps/
-chown -R ${htuser}:${htgroup} ${ncpath}/assets/
-chown -R ${htuser}:${htgroup} ${ncpath}/config/
-chown -R ${htuser}:${htgroup} ${ncpath}/data/
-chown -R ${htuser}:${htgroup} ${ncpath}/themes/
-chown -R ${htuser}:${htgroup} ${ncpath}/updater/
-
-chmod +x ${ncpath}/occ
-
-#printf "chmod/chown .htaccess\n"
-if [ -f ${ncpath}/.htaccess ]
- then
-  chmod 0644 ${ncpath}/.htaccess
-  chown ${rootuser}:${htgroup} ${ncpath}/.htaccess
-fi
-if [ -f ${ncpath}/data/.htaccess ]
- then
-  chmod 0644 ${ncpath}/data/.htaccess
-  chown ${rootuser}:${htgroup} ${ncpath}/data/.htaccess
-fi
+#!/usr/bin/env bash
+chmod +x /var/www/localhost/htdocs/nextcloud/occ
+chown -hR nginx:nginx /var/www/localhost/htdocs/nextcloud
+chown root:root /var/www/localhost/htdocs/nextcloud/.webapp*
