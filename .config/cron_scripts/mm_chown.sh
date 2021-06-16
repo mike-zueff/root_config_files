4a07d339feb9ec87af22a9212b48af3f72649be4
Mike Zueff <mike.zueff@gmail.com>
2021-06-16 08:16:59 +0300

    Use vmtouch script

--- .config/cron_scripts/mm_chown.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-. /root/.config/rc_files/principal_user.sh
-chown --no-dereference --recursive ${_RC_PRINCIPAL_USER}:${_RC_PRINCIPAL_USER} "/home/${_RC_PRINCIPAL_USER}/Nextcloud/nosync/mm/big_data" || true
