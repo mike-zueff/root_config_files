2d9f01ca3b25af2380507f2009f6c80989c3d301
Mike Zueff <mike.zueff@gmail.com>
2022-03-14 18:21:12 +0300

    Update cron_jobs_installer.sh

--- .config/cron_scripts/cron_jobs_installer.sh
+++ .config/cron_scripts/cron_jobs_installer.sh
@@ -4,5 +4,5 @@
 chmod u+x /root/.config/cron_scripts/local_delivery_agent.sh
 ln --force --symbolic /root/.config/cron_scripts/local_delivery_agent.sh /root/.config/cron_scripts/cron.minutely/local_delivery_agent.sh
 
-chmod u+x /root/.config/cron_scripts/vmtouch.sh
-ln --force --symbolic /root/.config/cron_scripts/vmtouch.sh /root/.config/cron_scripts/cron.minutely/vmtouch.sh
+# chmod u+x /root/.config/cron_scripts/vmtouch.sh
+# ln --force --symbolic /root/.config/cron_scripts/vmtouch.sh /root/.config/cron_scripts/cron.minutely/vmtouch.sh
