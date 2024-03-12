213f3354c0271f9d75cf87797643c4fde703c6d2
Mike Zueff <mike.zueff@gmail.com>
2024-03-12 16:40:43 +0300

    Update nc_reinstall_script.sh

--- nc_reinstall_script.sh
+++ nc_reinstall_script.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+echo Begin.
 chmod +x /var/www/localhost/htdocs/nextcloud/occ
 chown -hR nginx:nginx /var/www/localhost/htdocs/nextcloud
 chown root:root /var/www/localhost/htdocs/nextcloud/.webapp*
+echo End.
